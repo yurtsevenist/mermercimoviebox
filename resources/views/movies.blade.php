bf3d83965c4b4351489aea551c1e774a180e21ac
Feargithup <nazamarcimen@gmail.com>
2022-04-28 16:24:15 +0300

    ramazan-commit1

--- resources/views/movies.blade.php
+++ resources/views/movies.blade.php
@@ -6,7 +6,7 @@
 				<div class="row">
 					<div class="col-lg-12">
 						<div class="breadcrumb-area-content">
-							<h1>Filmler Sayfası</h1>
+							<h1>Ramazan değişiklik</h1>
 						</div>
 					</div>
 				</div>
