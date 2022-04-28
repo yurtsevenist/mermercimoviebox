1f99c0f71e44cc5c4bdb986f12ca5966fbd6e200
tunasahnx <h.tunasahin@gmail.com>
2022-04-28 16:18:47 +0300

    tuna2

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.master')
-@section('title','MovieBox-Anasayfa TunaDeğişim')
+@section('title','MovieBox-Anasayfa TunaDeğişim1')
 @section('content')
 		<!-- hero area start -->
 		<section class="hero-area" id="home">
